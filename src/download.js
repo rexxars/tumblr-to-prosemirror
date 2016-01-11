e2c691cf233e9f0daaf45ec61f97cf7c4888c24b
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-01-11 11:23:41 +0100

    Fetch all posts, not just first page

--- src/download.js
+++ src/download.js
@@ -1,19 +1,40 @@
 'use strict';
 
+var querystring = require('querystring');
 var request = require('request');
+var async = require('async');
 
 var url = 'http://api.tumblr.com/v2/blog/';
-var path = '/posts?api_key=';
+var path = '/posts';
 
 module.exports = function download(host, apiKey, callback) {
-    request({
-        url: url + host + path + apiKey,
-        json: true
-    }, function onTumblrResponse(err, res, body) {
-        if (err || res.statusCode !== 200) {
-            return callback(err || new Error('HTTP ' + res.statusCode));
-        }
+    var limit = 20;
+    var offset = 0 - limit;
+    var posts = [];
+    var lastChunkSize = 0;
 
-        callback(null, body.response.posts);
+    async.doWhilst(function downloadChunk(cb) {
+        offset += limit;
+
+        request({
+            url: url + host + path + '?' + querystring.stringify({
+                api_key: apiKey, // eslint-disable-line camelcase
+                limit: limit,
+                offset: offset
+            }),
+            json: true
+        }, function onTumblrResponse(err, res, body) {
+            if (err || res.statusCode !== 200) {
+                return cb(err || new Error('HTTP ' + res.statusCode));
+            }
+
+            lastChunkSize = body.response.posts.length;
+            posts = posts.concat(body.response.posts);
+            cb();
+        });
+    }, function shouldDownloadMore() {
+        return lastChunkSize === limit;
+    }, function onDownloadDone(err) {
+        callback(err, err ? null : posts);
     });
 };
