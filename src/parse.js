cc0f58f4b2a1477791be828234d2c3c4470f8422
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-01-28 09:39:55 +0100

    Depend on @bengler/prosemirror for now

--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var jsdom = require('jsdom').jsdom;
-var fromHTML = require('prosemirror/dist/format').fromHTML;
-var defaultSchema = require('prosemirror/dist/model').defaultSchema;
+var fromHTML = require('@bengler/prosemirror/dist/format').fromHTML;
+var defaultSchema = require('@bengler/prosemirror/dist/model').defaultSchema;
 
 var doc;
 
